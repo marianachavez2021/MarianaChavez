687b2205d1e6f93d4fd90b1246b52f5ae7766b73
Angel Caceres <caceres840@gmail.com>
2020-07-28 15:50:46 -0600

    Primer subida de codigo

---
+++ index.js
@@ -0,0 +1,190 @@
+//Importamos la libreria express
+var express = require("express");
+var app = express();
+
+//Le enseñamos a nuestro server a procesar JSON
+app.use(express.json());
+
+app.get("/students/:code", (request, response) => {
+  var code = request.params.code;
+  var student = {};
+  var statusCode = 200;
+
+  if (code == "06GC2108") {
+    //Retornar la informacion de Gabi Castro
+    student = {
+      name: "Gabriela Castro",
+      birthdate: "3/sep/2003",
+      age: 16,
+    };
+  } else if (code == "06KM2138") {
+    //Retornar informacion de Kate
+    student = {
+      name: "Kate Martinez",
+      birthdate: "30/jul/2003",
+      age: 16,
+    };
+  } else if (code == "06MV2277") {
+    //Retornar informacion de Melanie
+    student = {
+      name: "Melanie Valle",
+      birthdate: "02/feb/2004",
+      age: 16,
+    };
+  } else {
+    student = {
+      name: "Not found",
+    };
+    statusCode = 404;
+  }
+
+  response.status(statusCode).send(student);
+});
+
+app.post("/students", (request, response) => {
+  //Capturando la informacion del body
+  let studentData = request.body;
+
+  //Capturamos parte del JSON, Razon: Comprensión y orden
+  const studentPersonalData = studentData.personalData;
+  const studentGrades = studentData.grades;
+
+  //Asegurarnos que el alumnos esta en octavo o noveno
+  if (
+    studentPersonalData.academicGrade === "Octavo" ||
+    studentPersonalData.academicGrade === "Noveno"
+  ) {
+    //Iniciamos la variable que guardara la suma de las notas
+    let gradesSum = 0;
+
+    studentGrades.map((subject) => {
+      //Linea para suma acumulativa
+      gradesSum += subject.value;
+    });
+
+    /*
+      Promedio = sumaDeNotas / cantidadMaterias
+      Promedio = gradesSum / (studentGrades.length = 6)
+
+      length = Largo 
+      [ {}, {}, {} ].length = 3
+
+      parseFloat
+      parse -> Analizar o transformar
+      float = Numeros con decimales Ej. 34.00
+      Que tenga decimales
+
+      AVG = Average = Promedio
+    */
+    let AVG = parseFloat(gradesSum / studentGrades.length);
+
+    if (AVG >= 8) {
+      response.status(200).send({
+        message: "Bienvenido a Superate poma!",
+        average: AVG.toFixed(2), //Formato deseado -> nota.00 (Con dos decimales)
+      });
+    } else {
+      response.status(200).send({
+        message: "Siga participando",
+        cause: "Promedio debajo de 8, prom = " + AVG.toFixed(2),
+      });
+    }
+  } else {
+    response.status(200).send({
+      message: "No esta en el nivel adecuado",
+    });
+  }
+});
+
+app.put("/students/:code", (request, response) => {
+  let code = request.params.code;
+  let studentData = request.body
+
+  let exampleData = {
+    firstName: "Josue Fernando",
+    lastName: "Gomez Guardado",
+    personalData: {
+      age: 17,
+      height: 177,
+      weight: 140,
+      hairColor: "Black",
+      academicGrade: "Noveno",
+    },
+    parents: {
+      principalReponsable: {
+        completeName: "Elizabeth Guardado",
+        type: "mother",
+      },
+      secondayResponsable: {
+        completeName: "Hans Gomez",
+        type: "father",
+      },
+    },
+    economicStatus: {
+      liveInHouse: true,
+    },
+    family: {
+      hasBrothers: true,
+      manyBrothers: 2,
+    },
+    grades: [
+      {
+        name: "Math",
+        value: 8,
+      },
+      {
+        name: "Science",
+        value: 10,
+      },
+      {
+        name: "Language",
+        value: 5,
+      },
+      {
+        name: "History",
+        value: 7,
+      },
+      {
+        name: "Educacion fisica",
+        value: 10,
+      },
+      {
+        name: "Computacion",
+        value: 10,
+      },
+    ],
+  };
+
+  exampleData.firstName =  studentData.firstName
+  exampleData.lastName =  studentData.lastName
+
+  response.status(200).send(exampleData);
+});
+
+//app.use(express.urlencoded({ extended: false }))
+app.listen(3000, function () {
+  console.log("Cool Zone in port 3000!");
+});
+
+//Request -> Peticion = {  }
+//Response -> Respuesta = {  }
+//Formas de recibir informacion
+// 1. Query
+// 2. Params
+// 3. Body
+
+// 1 | 2 | 3
+// 3   1   3
+
+// resultado = (col1 * col2) / col3
+
+// 1. Analizar el problema
+//   1.1 Enfocarnos en lo que nos solicitan
+//   1.2 Formar una estrategia para resolver el problema
+// 2. Crear una solucion basica
+//   2.1 Identificar checkpoints
+//   2.2 Ser persistente
+//   2.3 Llegar a un solucion
+// 3. Llegar a una solucion estable
+//   3.1 Mejorar la solucion
+//   3.2 Terminar ejercicio
